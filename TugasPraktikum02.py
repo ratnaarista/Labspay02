c3c6999ae79fbc87f3cea879e790d2ff275ad115
ratnaarista <ratnaratnaa201@gmail.com>
2019-10-28 09:28:35 +0700

    coding

---
+++ TugasPraktikum02.py
@@ -0,0 +1,11 @@
+a = int(input("Masukkan bilangan A: "))
+b = int(input("Masukkan bilangan B: "))
+c = int(input("Masukkan bilangan C: "))
+
+if a > b and a > c:
+    print ( " Bilangan terbesar adalah: " , a)
+elif b > a and b > c:
+    print ( " Bilangan terbesar adalah: " , b)
+elif c > a and c > b:
+    print ( " Bilangan terbesar adalah: " , c)
+
